3f0b4bcf569033a8206e0aa10b7931e71bbbdc81
jeanfrySedano <jsedano.c98@gmail.com>
2026-07-19 18:48:52 -0300

    segunda corrccion

--- timeline-script.js
+++ timeline-script.js
@@ -69,7 +69,7 @@ function agregarTarjetaAlDOM(momento, index) {
             <span class="fecha-tag">${momento.fechaCreacion ? new Date(momento.fechaCreacion).toLocaleDateString('es-AR', { timeZone: 'UTC' }) : 'Sin fecha'}</span>
             <h3>${momento.autor}</h3>
             <p>${momento.texto}</p>
-            ${momento.foto ? `<img src="http://localhost:3000/uploads/${momento.foto}" style="max-width: 100%; border-radius: 8px; margin-top: 10px;">` : ''}
+            ${momento.foto ? `<img src="https://cami-jorge.onrender.com/uploads/${momento.foto}" style="max-width: 100%; border-radius: 8px; margin-top: 10px;">` : ''}
         </div>
     `;
     contenedor.prepend(item);
