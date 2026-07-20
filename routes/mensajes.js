144d65ed363d2ca5137bac057ee7e3e77e63261d
jeanfrySedano <jsedano.c98@gmail.com>
2026-07-19 21:30:27 -0300

    Limpieza de config Cloudinary

--- routes/mensajes.js
+++ routes/mensajes.js
@@ -6,11 +6,16 @@ const multer = require('multer'); // <--- 1. AGREGA ESTO
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true // Asegúrate de tener esto
 });
+
+
+
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
